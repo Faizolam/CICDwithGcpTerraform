08402ddd3c843b68d46bc7005a456e5297c41c5a
FaizOlam <faizpupil@gmail.com>
2024-11-01 00:25:04 +0530

    adding CICD Pipline

--- newBranch/backend.tf
+++ newBranch/backend.tf
@@ -1,22 +1,38 @@
 # backend config
-# Google reources
+
 terraform {
   required_version = "~> 1.5"
   backend "gcs" {
-    bucket = "backend-tf-stat" 
-    prefix = "terraform/ep1" 
-  }
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.0"
-    }
+    bucket  = "dl-youtube-state"
+    prefix  = "terraform/ep1"
   }
 }
 
 provider "google" {
-#   credentials = file(var.credentials_file)
-#   project     = var.project_id
-#   region      = var.region
+  #project = "dl-k8s-dev1cade"
 }
+
+
+
+# # backend config
+# # Google reources
+# terraform {
+#   required_version = "~> 1.5"
+#   backend "gcs" {
+#     bucket = "backend-tf-stat" 
+#     prefix = "terraform/ep1" 
+#   }
+
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+# #   credentials = file(var.credentials_file)
+# #   project     = var.project_id
+# #   region      = var.region
+# }
